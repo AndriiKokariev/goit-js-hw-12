99652f2a72aa7e4d6ed0add98ebb13937a6f434f
AndriiKokariev <andrewkok657@gmail.com>
2024-12-17 23:46:05 +0200

    final

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -15,19 +15,20 @@ export function gallery(images) {
         comments,
         downloads,
       }) => `
-      <a href="${largeImageURL}" class="gallery-item">
-        <img src="${webformatURL}" alt="${tags}" />
-        <div class="info">
-          <p>Likes: ${likes}</p>
-          <p>Views: ${views}</p>
-          <p>Comments: ${comments}</p>
-          <p>Downloads: ${downloads}</p>
-        </div>
-      </a>
-    `
+            <a href="${largeImageURL}" class="gallery-item">
+                <img src="${webformatURL}" alt="${tags}" />
+                <div class="info">
+                    <p>Likes: ${likes}</p>
+                    <p>Views: ${views}</p>
+                    <p>Comments: ${comments}</p>
+                    <p>Downloads: ${downloads}</p>
+                </div>
+            </a>
+        `
     )
     .join('');
-  g.innerHTML = element;
+  g.innerHTML += element;
+
   if (!box) {
     box = new SimpleLightbox('.gallery-item', {
       captionsData: 'alt',
@@ -40,4 +41,13 @@ export function gallery(images) {
 
 export function clear() {
   g.innerHTML = '';
+}
+
+export function loadMore(visible) {
+  const button = document.querySelector('#load-more');
+  if (visible) {
+    button.classList.remove('hidden');
+  } else {
+    button.classList.add('hidden');
+  }
 }
